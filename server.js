fcf6231b397c310f1b1e7b2a2022c16c682a6288
Desiree Tien <desireetien@Aphesis.local>
2017-12-19 12:58:02 -0600

    Adding package.json

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static("app/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 app.listen(PORT, function() {
 	console.log("connected on port " + PORT);
