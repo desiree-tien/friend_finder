932f65c18e2e35161c031d716680b842d52d4ec3
Desiree Tien <desireetien@Aphesis.local>
2017-12-19 15:26:49 -0600

    Updaging fake friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,7 @@ var friends = [
 		scores: [1, 3, 2, 4, 5,2 , 4, 1, 5,2]
 	},
 	{
-		name: 'Scott',
+		name: 'Meg',
 		photo: 'https://media1.popsugar-assets.com/files/thumbor/4NjIcVVSARGq-XaSkF-cr3HAcqM/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/04/24/816/n/1922283/2908edfc2d2b9ebc_thumb_temp_image346515061398361011/i/Meg-Ryan-Movie-Clips.jpg',
 		scores: [1, 3, 2, 4, 5,2 , 4, 1, 5,2]
 	}
